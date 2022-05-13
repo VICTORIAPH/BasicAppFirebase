e4a98865e5524e101a4d7e548c4fc942a0c4ad38
victo <vicky454ph@gmail.com>
2022-05-12 22:48:24 -0500

    conectamos a la bd

---
+++ app/src/main/java/com/example/appfirebase/Card.kt
@@ -0,0 +1,8 @@
+package com.example.appfirebase
+
+class Card(valor: String, color: String,  palo: String) {
+    val valor = valor
+    val color = color
+    val palo = palo
+
+}
--- app/src/main/java/com/example/appfirebase/MainActivity.kt
+++ app/src/main/java/com/example/appfirebase/MainActivity.kt
@@ -2,10 +2,26 @@ package com.example.appfirebase
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.google.firebase.database.ktx.database
+import com.google.firebase.ktx.Firebase
+
+
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val database = Firebase.database
+        val myRef  = database.reference
+
+        myRef.child("carta" ).child("1").setValue(Card("1", "rosa", "diamantes"))
+         myRef.child("carta" ).child("2").setValue(Card("2", "negro", "corazones"))
+        myRef.child("carta" ).child("3").setValue(Card("3", "rojo", "diamantes"))
+
+
+
+
+
     }
 }
